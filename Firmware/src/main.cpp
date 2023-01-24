292536e2400fccbaa0ebb5ec7db622e3395af3ab
Juan Vivas <j.vivas@dea-ht.com>
2023-01-24 13:39:21 +0100

    Connects to a remote server to get current time

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 //https://randomnerdtutorials.com/esp32-web-server-arduino-ide/
 #include <DS18B20.h>
+#include "time.h"
 
 #define LOW_ALARM 20
 #define HIGH_ALARM 25
@@ -52,6 +53,45 @@ byte pos = 0;
 #define INPUT_PIN3 27
 #define INPUT_PIN4 14
 
+//time
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
 
 void setup() {
 
@@ -91,7 +131,15 @@ void setup() {
   Serial.println(WiFi.localIP());
   server.begin();
 
+
+    // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+
 }
+
+
 
 
 void clear() {
